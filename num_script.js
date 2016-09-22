7571e2cd7d67872900e9fa9d2e8a247828aeb011
Robert Schneiderman <Rob@Roberts-MacBook-Pro-2.local>
2016-09-22 11:22:10 -0700

    putting numscript on hold

--- num_script.js
+++ num_script.js
@@ -1,26 +1,26 @@
-chrome.runtime.onMessage.addListener(message => {
-  let title = document.querySelector("title")
-  switch (message.type) {
-    case "PREPEND_TITLE":
-      title.innerHTML = `${message.tab.index + 1} ${message.tab.title}`
-      break;
+// chrome.runtime.onMessage.addListener(message => {
+//   let title = document.querySelector("title")
+//   switch (message.type) {
+//     case "PREPEND_TITLE":
+//       title.innerHTML = `${message.tab.index + 1} ${message.tab.title}`
+//       break;
 
-    case "SHORTEN_TITLE":
-      title.innerHTML = `${message.tab.title}`.slice(2);
-      break;
-    default:
-  }
-});
+//     case "SHORTEN_TITLE":
+//       title.innerHTML = `${message.tab.title}`.slice(2);
+//       break;
+//     default:
+//   }
+// });
 
 
-document.addEventListener('keydown', (e) => {
-  if (e.which === 18) {
-    chrome.runtime.sendMessage(null, {type: 'SHOW_NUMBERS'})
-  }
-});
+// document.addEventListener('keydown', (e) => {
+//   if (e.which === 18) {
+//     chrome.runtime.sendMessage(null, {type: 'SHOW_NUMBERS'})
+//   }
+// });
 
-document.addEventListener('keyup', (e) => {
-  if (e.which === 18) {
-    chrome.runtime.sendMessage(null, {type: 'REMOVE_NUMBERS'});
-  }
-});
+// document.addEventListener('keyup', (e) => {
+//   if (e.which === 18) {
+//     chrome.runtime.sendMessage(null, {type: 'REMOVE_NUMBERS'});
+//   }
+// });
